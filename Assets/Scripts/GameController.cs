994511b7e98bcc0beb0a6e852f8c10eb98b4e5df
FranthescoAraujo <89648233+FranthescoAraujo@users.noreply.github.com>
2022-04-28 00:49:17 -0300

    Primeiro commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] characters;
+    public List<GameObject> charactersOnScreen;
+
+
+    private void Start()
+    {
+        characters = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    private void Update()
+    {
+        FindGameObjectsOnScreen();
+    }
+
+    private void FindGameObjectsOnScreen()
+    {
+        foreach (var character in characters)
+        {
+            if (character.GetComponentsInChildren<Renderer>(true)[0].enabled)
+            {
+                if (charactersOnScreen.IndexOf(character) == -1)
+                {
+                    charactersOnScreen.Add(character);
+                }
+            }
+        }
+    }
+
+    private void Fight()
+    {
+        if (charactersOnScreen.Count == 2)
+        {
+            if (charactersOnScreen[0].GetComponent<Personagem>().forca > charactersOnScreen[1].GetComponent<Personagem>().forca)
+            {
+                charactersOnScreen[0].GetComponentInChildren<Animator>().Play("Atack");
+                charactersOnScreen[1].GetComponentInChildren<Animator>().Play("Death");
+                return;
+            }
+            charactersOnScreen[0].GetComponentInChildren<Animator>().Play("Death");
+            charactersOnScreen[1].GetComponentInChildren<Animator>().Play("Atack");
+            return;
+        }
+        if (charactersOnScreen.Count == 1)
+        {
+            charactersOnScreen[0].GetComponentInChildren<Animator>().Play("Idle");
+            return;
+        }
+    }
+}
